23b425c2701992fb3dbf2b35e5e273f41845b888
Jan Bizoń <jbizon@griddynamics.com>
2024-12-18 09:42:18 +0100

    Shell check and input verfication for word processor.

--- 5.word_processor.sh
+++ 5.word_processor.sh
@@ -1,71 +1,69 @@
 #!/bin/bash 
 # Word processor
 #
-# Not checking if file exist
-# Diffrent arguments to clear
-#
 # Example use:
 # ./word_processor.sh -r -l -i message.txt -o converted.txt -s Hello "Good Morning"
 
 set -e
 
 # Variables
-LFLAG=false
-RFLAG=false
-UFLAG=false
-VFLAG=false
-SFLAG=false
+lowerFlag=false
+reverseFlag=false
+upperFlag=false
+flipCaseFlag=false
+substituteFlag=false
+OPTSTRING=":rluvs:i:o:"
 
 # Functions
-check_if_empty () {
-    if [[ -n $1 ]] ; then
-        echo $1
+take_not_empty () {
+    if [[ -n "$1" ]] ; then
+        echo "$1"
     else
-        echo $2
+        echo "$2"
     fi
 }
 
 
 # Input processing
-while getopts "rluvs:i:o:"  OPT ; do
-    case "${OPT}" in
+while getopts ${OPTSTRING} opt ; do
+    case "${opt}" in
     i)
         INPUT=${OPTARG}
-        if [ ! -f $INPUT ] ; then 
+        if [ ! -f "$INPUT" ] ; then 
             echo Wrong input file: "${INPUT}"
             exit 1
         fi
     ;;
     o)
         OUTPUT=${OPTARG}
-        if [ ! -f $OUTPUT ] ; then 
+        if [ ! -f "$OUTPUT" ] ; then 
             echo Wrong output file: "${OUTPUT}"
             exit 1
         fi
     ;;
     r)
-        RFLAG=true
+        reverseFlag=true
     ;;
     l)
-        LFLAG=true
-        if [ $UFLAG = true ]  ; then
-            UFLAG=false
+        lowerFlag=true
+        if [ $upperFlag = true ]  ; then
+            upperFlag=false
             echo Lowercase is being used
         fi
     ;;
     u)
-        UFLAG=true
-        if [ $LFLAG = true ]; then
-            LFLAG=false
+        upperFlag=true
+        if [ $lowerFlag = true ]; then
+            lowerFlag=false
             echo Uppercase is being used
         fi
     ;;
     v)
-        VFLAG=true
+        flipCaseFlag=true
     ;;
     s)
-        SFLAG=true
-        shift $[ $OPTIND - 1 ] # changing pointer on input - then there is known that 2 argument for s is $1
+        substituteFlag=true
+        shift $(( OPTIND - 1 )) # changing pointer on input - then there is known that 2nd argument for s is $1
         AWORD=${OPTARG}
         BWORD=$1
         shift 1                 # skipping $1 for furthure search
@@ -76,53 +74,62 @@ while getopts "rluvs:i:o:"  OPT ; do
      ;;
     :)
         echo "Option -$OPTARG requires an argument."
+        exit 1
      ;;
     esac
 done
 
+if [ -z "$INPUT" ] || [ -z "$OUTPUT" ] ; then
+    echo "No input or output specified, use -o and -i with filenames."
+    exit 1
+fi
+
+
+
 # Main Program
-echo -n "" > ${OUTPUT}
+# ====================================================
+echo -n "" > "${OUTPUT}"
 while read -r SINGLELINE; do
-   CONV_TEXT=$SINGLELINE
-   OUTPUT_TEXT=""
-    if [ $SFLAG = true ] ; then
-            OUTPUT_TEXT=`echo "${CONV_TEXT}" | awk -v AWORD="${AWORD}" -v BWORD="${BWORD}" '{ gsub( AWORD, BWORD ); print}'`
+   conv_text=$SINGLELINE
+   last_step_text=""
+    if [ $substituteFlag = true ] ; then
+            last_step_text=$(echo "${conv_text}" | awk -v AWORD="${AWORD}" -v BWORD="${BWORD}" '{ gsub( AWORD, BWORD ); print}')
     fi
-    CONV_TEXT=`check_if_empty "$OUTPUT_TEXT" "$SINGLELINE"`
-    if [ $RFLAG  = true ] ; then
+    conv_text=$(take_not_empty "$last_step_text" "$SINGLELINE")
+    if [ $reverseFlag  = true ] ; then
        #N={#SINGLELINE}
-       N=`wc -c <<< "${CONV_TEXT}"`
-       OUTPUT_TEXT=""
-       for((i=$N-1;i>=0;i--)); do
-            OUTPUT_TEXT="$OUTPUT_TEXT${CONV_TEXT:$i:1}"
+       N=$(wc -c <<< "${conv_text}")
+       last_step_text=""
+       for((i=N-1 ; i>=0 ; i--)); do
+            last_step_text="$last_step_text${conv_text:$i:1}"
         done
     fi
-    CONV_TEXT=`check_if_empty "$OUTPUT_TEXT" "$SINGLELINE"`
-    if [ $LFLAG = true ] ; then
-            OUTPUT_TEXT=`echo "${CONV_TEXT}" | awk '{print tolower( $0 )}'`
+    conv_text=$(take_not_empty "$last_step_text" "$SINGLELINE")
+    if [ $lowerFlag = true ] ; then
+            last_step_text=$(echo "${conv_text}" | awk '{print tolower( $0 )}')
     fi
-    CONV_TEXT=`check_if_empty "$OUTPUT_TEXT" "$SINGLELINE"`
-    if [ $UFLAG = true ] ; then
-            OUTPUT_TEXT=`echo "${CONV_TEXT}" | awk '{print toupper( $0 )}'`
+    conv_text=$(take_not_empty "$last_step_text" "$SINGLELINE")
+    if [ $upperFlag = true ] ; then
+            last_step_text=$(echo "${conv_text}" | awk '{print toupper( $0 )}')
     fi
-    CONV_TEXT=`check_if_empty "$OUTPUT_TEXT" "$SINGLELINE"`
-    if [ $VFLAG = true ] ; then
-       N=`wc -c <<< "${CONV_TEXT}"`
-       OUTPUT_TEXT=""
-       for i in `seq 0 $N`; do
-            CH=${CONV_TEXT:$i:1}
-            case $CH in
+    conv_text=$(take_not_empty "$last_step_text" "$SINGLELINE")
+    if [ $flipCaseFlag = true ] ; then
+       N=$(wc -c <<< "${conv_text}")
+       last_step_text=""
+       for _ in $(seq 0 "$N"); do
+            ch=${conv_text:$i:1}
+            case $ch in
             [[:lower:]])
-                CH=`echo ${CH} | awk '{print toupper($0) }'`
+                ch=$( echo "${ch}" | awk '{print toupper($0) }' )
                 ;;
             [[:upper:]])
-                CH=`echo ${CH} | awk '{print tolower($0) }'`
+                ch=$( echo "${ch}" | awk '{print tolower($0) }' )
                 ;;
             esac
-            OUTPUT_TEXT="${OUTPUT_TEXT}${CH}" 
+            last_step_text="${last_step_text}${ch}" 
        done
     fi
-    CONV_TEXT=`check_if_empty "$OUTPUT_TEXT" "$SINGLELINE"`
-    echo ${OUTPUT_TEXT} >> ${OUTPUT}
-done <${INPUT}
+    conv_text=$(take_not_empty "$last_step_text" "$SINGLELINE")
+    echo "${last_step_text}" >> "${OUTPUT}"
+done < "${INPUT}"
 
