7b67f2094ac1b7d939243a6795d91de993e34567
Jan Bizoń <jbizon@griddynamics.com>
2024-12-18 09:07:28 +0100

    Caesar cipher complete rework and testing - error handling.

--- 4.caesar_cipher.sh
+++ 4.caesar_cipher.sh
@@ -1,18 +1,19 @@
 #!/bin/bash 
 # Script to cipher wiht caesar cipher.
 #
-# There are no spaces - spaces are changed into -
+# Example : ./caesar_cipher.sh -i message.txt -o cipher.txt -s 10
 #
 set -u
 
 # Variables
-ALPH_SIZE=58                    # A-z
-OPTSTRING="i:o:s:v-:"
-MINCHAR=65                         # eq A in ASCII
-Z=90                             # Number in ASCII
-a=97
+OPTSTRING=":i:o:s:v-:"
+A=65                            # eq A in ASCII
 z=122
-SUPPRESS=false
+ALPH_SIZE=26                    # a-z range size
+suppress=false
+INPUT=
+OUTPUT=
+SHIFT=
 
 # Input processing
 while getopts ${OPTSTRING} opt; do
@@ -20,29 +21,31 @@ while getopts ${OPTSTRING} opt; do
     -)
         case "${OPTARG}" in
             suppress)
-                SUPPRESS=true
-                #OPTIND=$(( OPTIND + 1 ))
+                suppress=true
                 echo Output is suppressed
+                ;;
+                \?)
+                     echo "Invalid option: -$OPTARG"
                 ;;
         esac
         ;;
     i)
         INPUT=${OPTARG}
-        if [ ! -f $INPUT ] ; then 
+        if [ ! -f "$INPUT" ] ; then 
             echo Wrong input file: "${INPUT}"
             exit 1
         fi
     ;;
     o)
         OUTPUT=${OPTARG}
-        if [ ! -f $OUTPUT ] ; then 
+        if [ ! -f "$OUTPUT" ] ; then 
             echo Wrong output file: "${OUTPUT}"
             exit 1
         fi
     ;;
     s)
         SHIFT=${OPTARG}
-        if [ $SHIFT -le 0 ] ; then
+        if [ "$SHIFT" -le 0 ] ; then
             echo Shift should be '>' 0
         fi
     ;;
@@ -56,50 +59,45 @@ while getopts ${OPTSTRING} opt; do
     ;;
     :)
         echo "Option -$OPTARG requires an argument."
+        exit 1
     ;; 
     esac
 done
 
+if [ -z "$INPUT" ] || [ -z "$OUTPUT" ] || [ -z "$SHIFT" ] ; then
+    echo Lacking at least one of arguments: execute with -v for more info. 
+    exit 1
+fi
+
+
 # Main program
-echo -n ""  > ${OUTPUT}
-shopt -s extglob                            # Turn on globbing for case
-while read -r SINGLELINE; do 
-    # N=`expr "${SINGLELINE}" : '.*'`       # expr is problematic
-    N=`wc -c <<< "${SINGLELINE}"`
-    SINGLELINE=`tr ' ' '-' <<< "${SINGLELINE}"`
-    #ARR=(`fold -w 1 <<< ${SINGLELINE}`)
-    ARR=(`grep -o . <<< "${SINGLELINE}"`)
-    for i in ${ARR[@]}; do
-        if [[ $i == [[:alpha:]] ]] ; then
-            TOCHAR=`printf '%d' "'$i"`
-            # Linear space cut off
-            CIPH=$(( ( ( ${TOCHAR} - ${MINCHAR} + ${SHIFT} ) % ${ALPH_SIZE} ) + ${MINCHAR} ))
 
-            # Reinterpret characters over legal limitations
-            if [ $CIPH -gt $Z ]  && [ $CIPH -lt $a ]; then
-                CIPH=$(( ( ${CIPH} % $(($Z + 1)) + $a ) ))
-            fi
-            if [ $CIPH -gt $z ] ; then
-                CIPH=$(( ( ${CIPH} % $z ) + ${MINCHAR} ))
-            fi
+STDOUT=/dev/stdout
+if [ $suppress = true ]; then
+    STDOUT=/dev/null
+fi
 
-            if [ $SUPPRESS = false ] ; then
-                printf "\x`printf %x ${CIPH}`" | tee -a "${OUTPUT}"
-            else
-                printf "\x`printf %x ${CIPH}`" >> ${OUTPUT} 
-            fi
-        else 
-            if [ $SUPPRESS = false ] ; then
-                printf %c $i | tee -a "${OUTPUT}"
-            else
-                printf %c $i >> ${OUTPUT}
-            fi
-        fi
-    done
-    if [ $SUPPRESS = false ] ; then
-        printf '\n' | tee -a "${OUTPUT}"
-    else
-        printf '\n' >> ${OUTPUT}
-    fi
-done <${INPUT}
+awk -v low=$A -v high=$z -v shift="$SHIFT" -v alph_size="$ALPH_SIZE" '
+BEGIN {
+for (i = low; i <= high; i++) {
+        t = sprintf("%c", i)
+        _ord_[t] = i
+    }
+};
+{ 
+split($0, chars, "");
+for (i=1; i <= length($0); i++) {
+    character = chars[i]  
+    if( character ~ /[A-Z]/) {
+        ciph = ((_ord_[character] - _ord_["A"] + shift ) % alph_size ) + _ord_["A"];
+    } else if( character ~ /[a-z]/) {
+        ciph = ((_ord_[character] - _ord_["a"] + shift ) % alph_size ) + _ord_["a"];
+    } else {
+        ciph = character;
+    }
+    printf("%c", ciph);
+
+}
+    print ""
+} ' "${INPUT}" | tee "${OUTPUT}" > "${STDOUT}"
 
